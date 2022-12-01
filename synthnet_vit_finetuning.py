6dbc83db48549a923e55187b1a016271fc9639c6
Dennis Ritter <dennis.ritter@bht-berlin.de>
2022-12-01 17:34:30 +0100

    python script for finetuning and hp_search

---
+++ synthnet_vit_finetuning.py
@@ -0,0 +1,326 @@
+import os
+import time
+from types import SimpleNamespace
+import numpy as np
+from datasets import load_dataset
+from transformers import ViTFeatureExtractor, ViTForImageClassification, TrainingArguments, Trainer
+import evaluate
+import torch
+from torchvision.transforms import (CenterCrop, Compose, Normalize, RandomHorizontalFlip, RandomVerticalFlip, Resize, ToTensor)
+# import torchvision.transforms as transforms
+# from PIL import Image
+import click
+import wandb
+
+
+@click.command()
+@click.option(
+    '--project_name',
+    help="Project name this run belongs to. (WandB Project)",
+    type=str,
+    required=True,
+)
+@click.option(
+    '--output_dir',
+    help='Output directory path. Timestamp will be appended on runtime.',
+    type=click.Path(),
+    required=True,
+)
+@click.option(
+    '--train_ds',
+    help="Training Dataset in Huggingface imagefolder structure.",
+    type=click.Path(exists=True, file_okay=False, dir_okay=True),
+    required=True,
+)
+@click.option(
+    '--val_ds',
+    help="Validation Dataset in Huggingface imagefolder structure.",
+    type=click.Path(exists=True, file_okay=False, dir_okay=True),
+    required=True,
+)
+@click.option(
+    '--test_ds',
+    help="Test Dataset in Huggingface imagefolder structure.",
+    type=click.Path(exists=True, file_okay=False, dir_okay=True),
+    required=True,
+)
+@click.option(
+    '--run_name',
+    help="Name for this training run. Timestamp will be appended on runtime.",
+    type=str,
+    required=False,
+)
+@click.option(
+    '--mode',
+    help="Either HP_Search or Finetuning mode.",
+    type=click.Choice(['HP_SEARCH', 'FINETUNING']),
+    required=False,
+)
+@click.option(
+    '--checkpoint',
+    help="Path to a pretrained checkpoint to load.",
+    type=click.Path(exists=True, file_okay=True, dir_okay=False),
+    required=False,
+)
+@click.option(
+    '--seed',
+    help='random seed',
+    type=int,
+    show_default=True,
+    default=42,
+)
+@click.option(
+    '--num_train_epochs',
+    help='TrainArguments.num_train_epochs',
+    type=int,
+    show_default=True,
+    default=3,
+)
+@click.option(
+    '--per_device_train_batch_size',
+    help='TrainArguments.per_device_train_batch_size',
+    type=int,
+    show_default=True,
+    default=16,
+)
+@click.option(
+    '--per_device_eval_batch_size',
+    help='TrainArguments.per_device_eval_batch_size',
+    type=int,
+    show_default=True,
+    default=16,
+)
+@click.option(
+    '--learning_rate',
+    help='TrainArguments.learning_rate',
+    type=float,
+    show_default=True,
+    default=2e-5,
+)
+@click.option(
+    '--weight_decay',
+    help='TrainArguments.weight_decay',
+    type=float,
+    show_default=True,
+    default=1e-2,
+)
+@click.option(
+    '--search_hyperparams',
+    help='if true: search hyperparameters with backed in sweep_config (wandb). Might ignore several other config-args given.',
+    type=bool,
+    show_default=True,
+    default=False,
+)
+def main(**kwargs):
+    # Parse click parameters and load config
+    args = SimpleNamespace(**kwargs)
+
+    ###############################################################
+    ## LOADING DATA
+    ###############################################################
+
+    # load datasets from image directory (huggingface)
+    # https://huggingface.co/docs/datasets/image_dataset
+    # Ensure format ds_dir/label/file.png
+    train_ds = load_dataset("imagefolder", data_dir=args.train_ds, split="train")
+    val_ds = load_dataset("imagefolder", data_dir=args.val_ds, split="train")
+    test_ds = load_dataset("imagefolder", data_dir=args.test_ds, split="test")
+    # map ids to labels and vice versa
+    id2label = {id: label for id, label in enumerate(train_ds.features['label'].names)}
+    label2id = {label: id for id, label in id2label.items()}
+
+    ###############################################################
+    ## PREPROCESSING DATA
+    ###############################################################
+
+    # load huggingface feature extractor to prepare data for the model
+    feature_extractor = ViTFeatureExtractor.from_pretrained("google/vit-base-patch16-224-in21k")
+    # Define Transforms
+    normalize = Normalize(mean=feature_extractor.image_mean, std=feature_extractor.image_std)
+    _train_transforms = Compose([
+        Resize(feature_extractor.size),
+        RandomHorizontalFlip(),
+        RandomVerticalFlip(),
+        ToTensor(),
+        normalize,
+    ])
+
+    _val_transforms = Compose([
+        Resize(feature_extractor.size),
+        CenterCrop(feature_extractor.size),
+        ToTensor(),
+        normalize,
+    ])
+
+    def train_transforms(examples):
+        examples['pixel_values'] = [_train_transforms(image.convert("RGB")) for image in examples['image']]
+        return examples
+
+    def val_transforms(examples):
+        examples['pixel_values'] = [_val_transforms(image.convert("RGB")) for image in examples['image']]
+        return examples
+
+    # Set the transforms
+    train_ds.set_transform(train_transforms)
+    val_ds.set_transform(val_transforms)
+    test_ds.set_transform(val_transforms)
+
+    def collate_fn(examples):
+        pixel_values = torch.stack([example["pixel_values"] for example in examples])
+        labels = torch.tensor([example["label"] for example in examples])
+        return {"pixel_values": pixel_values, "labels": labels}
+
+    # define training eval metric
+    metric = evaluate.load("accuracy")
+
+    def compute_metrics(eval_pred):
+        predictions, labels = eval_pred
+        predictions = np.argmax(predictions, axis=1)
+        return metric.compute(predictions=predictions, references=labels)
+
+    ###############################################################
+    ## DEFINE MODEL AND TRAINING CONFIGURATIONS
+    ###############################################################
+
+    num_labels = len(id2label.keys())
+    model = ViTForImageClassification.from_pretrained('google/vit-base-patch16-224-in21k', num_labels=num_labels, id2label=id2label, label2id=label2id)
+
+    def train_finetune(
+        run_name: str,
+        output_dir: str,
+        seed: int,
+        num_train_epochs: int,
+        per_device_train_batch_size: int,
+        per_device_eval_batch_size: int,
+        learning_rate: float,
+        weight_decay: float,
+        checkpoint: str,
+    ):
+        wandb.init(project=args.project_name)
+        if run_name:
+            wandb.run.name = run_name
+        # define train arguments
+        timestamp = time.strftime("%y%m%d%M%S", time.gmtime())
+        training_args = TrainingArguments(
+            # run_name=run_name,
+            output_dir=f"{output_dir}/{wandb.run.name}",
+            seed=seed,
+            save_strategy="epoch",
+            evaluation_strategy="epoch",
+            logging_strategy='epoch',
+            num_train_epochs=num_train_epochs,
+            per_device_train_batch_size=per_device_train_batch_size,
+            per_device_eval_batch_size=per_device_eval_batch_size,
+            learning_rate=learning_rate,
+            weight_decay=weight_decay,
+            optim="adamw_torch",
+            fp16=True,
+            # adam_beta1=0.9,
+            # adam_beta2=0.999,
+            # adam_epsilon=1e-8,
+            # max_grad_norm=1.0,
+            # dataloader_num_workers=0,
+            disable_tqdm=False,
+            load_best_model_at_end=True,
+            metric_for_best_model="accuracy",
+            remove_unused_columns=False,
+            report_to="wandb")
+
+        # define trainer and start training loop
+        trainer = Trainer(
+            model,
+            training_args,
+            train_dataset=train_ds,
+            eval_dataset=val_ds,
+            data_collator=collate_fn,
+            compute_metrics=compute_metrics,
+            tokenizer=feature_extractor,
+        )
+        if args.checkpoint:
+            trainer.train(resume_from_checkpoint=checkpoint)
+        else:
+            trainer.train()
+
+        ###############################################################
+        ## EVALUATION
+        ###############################################################
+        outputs = trainer.predict(test_ds)
+        print(f"{outputs.metrics=}")
+
+    def train_hyperparam_search(config=None):
+        with wandb.init(config=config):
+            # set sweep configuration
+            config = wandb.config
+
+            # set training arguments
+            training_args = TrainingArguments(output_dir=f'{args.output_dir}',
+                                              report_to='wandb',
+                                              num_train_epochs=config.epochs,
+                                              learning_rate=config.learning_rate,
+                                              weight_decay=config.weight_decay,
+                                              per_device_train_batch_size=config.batch_size,
+                                              per_device_eval_batch_size=16,
+                                              save_strategy='epoch',
+                                              evaluation_strategy='epoch',
+                                              logging_strategy='epoch',
+                                              load_best_model_at_end=True,
+                                              remove_unused_columns=False,
+                                              fp16=True)
+
+            # define training loop
+            trainer = Trainer(
+                model,
+                training_args,
+                train_dataset=train_ds,
+                eval_dataset=val_ds,
+                data_collator=collate_fn,
+                compute_metrics=compute_metrics,
+                tokenizer=feature_extractor,
+            )
+
+            # start training loop
+            trainer.train()
+
+    if args.mode == "FINETUNING":
+        train_finetune(
+            run_name=args.run_name,
+            output_dir=args.output_dir,
+            seed=args.seed,
+            num_train_epochs=args.num_train_epochs,
+            per_device_train_batch_size=args.per_device_train_batch_size,
+            per_device_eval_batch_size=args.per_device_eval_batch_size,
+            learning_rate=args.learning_rate,
+            weight_decay=args.weight_decay,
+            checkpoint=args.checkpoint,
+        )
+
+    if args.mode == "HP_SEARCH":
+        train_ds = train_ds.train_test_split(train_size=0.2, stratify_by_column='label')['train']
+        val_ds = val_ds.train_test_split(test_size=0.2, stratify_by_column='label')['test']
+
+        # method
+        sweep_config = {'method': 'random'}
+        # hyperparameters
+        parameters_dict = {
+            'epochs': {
+                'value': 20
+            },
+            'batch_size': {
+                'values': [8, 16, 32, 64]
+            },
+            'learning_rate': {
+                'distribution': 'log_uniform_values',
+                'min': 1e-5,
+                'max': 1e-4
+            },
+            'weight_decay': {
+                'values': [0.01, 0.1]
+            },
+        }
+        sweep_config['parameters'] = parameters_dict
+        sweep_id = wandb.sweep(sweep_config, project=args.project_name)
+        wandb.agent(sweep_id, train_hyperparam_search, count=20)
+
+
+if __name__ == "__main__":
+    main()
