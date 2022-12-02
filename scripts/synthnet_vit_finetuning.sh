fa94001b4adce2fb8c5c796e267218f1d234745e
Dennis Ritter <dennis.r.ritter@gmail.com>
2022-12-02 14:13:56 +0100

    scripts

--- scripts/synthnet_vit_finetuning.sh
+++ scripts/synthnet_vit_finetuning.sh
@@ -2,8 +2,8 @@
 PROJECT_NAME='synthnet-vit-finetuning'
 OUTPUT_DIR='out/synthnet_finetuning/vit' # + RUN_NAME on runtime
 
-TRAIN_DS='data/7054-12-300-l_drucker_se_su_st_st_512_32' # todo
-TEST_DS='data/topex-real-123_pb_256' # todo
+TRAIN_DS='data/topex-printer/train'
+TEST_DS='data/topex-printer/test'
 SPLIT_VAL_SIZE=0.1
 
 MODE='FINETUNING'
--- scripts/synthnet_vit_hyperparameter_search.sh
+++ scripts/synthnet_vit_hyperparameter_search.sh
@@ -3,19 +3,16 @@ PROJECT_NAME='synthnet-vit-hyperparameters'
 # PROJECT_NAME='synthnet-vit-hyperparameters'
 OUTPUT_DIR='out/synthnet_hyperparameter_search/vit' # + RUN_NAME on runtime
 
-TRAIN_DS='data/7054-12-300-l_drucker_se_su_st_st_512_32'
-VAL_DS='data/7054-12-300-l_drucker_se_su_st_st_512_32'
-TEST_DS='data/topex-real-123_pb_256'
-
+TRAIN_DS='data/topex-printer/train'
+TEST_DS='data/topex-printer/test'
 MODE='HP_SEARCH'
+EPOCHS=10
 
-EPOCHS=3
-
-python synthnet_vit_finetune.py \
+python synthnet_vit_finetuning.py \
 --project_name $PROJECT_NAME \
 --output_dir $OUTPUT_DIR \
 --train_ds $TRAIN_DS \
---val_ds $VAL_DS \
 --test_ds $TEST_DS \
 --mode $MODE \
---num_train_epochs $EPOCHS
+--num_train_epochs $EPOCHS \
+--hps_run_count 10
