c28bcd7bdb1137a71f57c17081142d45e9528c48
Dennis Ritter <dennis.ritter@bht-berlin.de>
2023-01-02 14:54:09 +0100

    adjust visda runs; renaming projects

--- scripts/visda2017_da.sh
+++ scripts/visda2017_da.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
-PROJECT_NAME='visda2017-vit-finetuning'
-RUN_NAME='vitb16_augmix'
-OUTPUT_DIR='out/synthnet_finetuning/synthnet_finetuning/vit'
+DATASET="visda2017"
 
-TRAIN_DS='data/visda2017/train'
-# VAL_DS='data/visda2017/val'
-TEST_DS='data/visda2017/test'
+PROJECT_NAME="${DATASET}_DA"
+RUN_NAME="vitb16_in21k_augmix"
+OUTPUT_DIR="out/synthnet_finetuning/${PROJECT_NAME}"
 
-MODE='FINETUNING'
+TRAIN_DS="data/${DATASET}/train"
+VAL_DS="data/${DATASET}/val"
+TEST_DS="data/${DATASET}/test"
+
+MODE="FINETUNING"
 EPOCHS=20
 BATCH_SIZE=16
 LR=3e-5
@@ -15,7 +17,7 @@ WEIGHT_DECAY=0.1
 WARM_UP_RATIO=0.1
 
 python synthnet_vit_finetuning.py \
---model 'google/vit-base-patch16-224-in21k'
+--model "google/vit-base-patch16-224-in21k" \
 --project_name $PROJECT_NAME \
 --run_name $RUN_NAME \
 --output_dir $OUTPUT_DIR \
