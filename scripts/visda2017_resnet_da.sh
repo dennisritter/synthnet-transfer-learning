c1f04f036d3f5300933a37c8dcb919e7bbff2b42
Dennis Ritter <dennis.ritter@bht-berlin.de>
2023-01-03 17:21:35 +0100

    resnet lp augmix

--- scripts/visda2017_resnet_da.sh
+++ scripts/visda2017_resnet_da.sh
@@ -2,7 +2,7 @@
 DATASET="visda2017"
 
 PROJECT_NAME="${DATASET}_DA"
-RUN_NAME="resnet_in1k"
+RUN_NAME="resnet_in1k_augmix_lp"
 OUTPUT_DIR="out/synthnet_finetuning/${PROJECT_NAME}"
 
 TRAIN_DS="data/${DATASET}/train"
@@ -23,7 +23,7 @@ python synthnet_vit_finetuning.py \
 --val_ds $VAL_DS \
 --test_ds $TEST_DS \
 --run_name $RUN_NAME \
---train_layers "FULL" \
+--train_layers "CLASS_HEAD" \
 --seed 42 \
 --batch_size $BATCH_SIZE \
 --num_train_epochs $EPOCHS \
