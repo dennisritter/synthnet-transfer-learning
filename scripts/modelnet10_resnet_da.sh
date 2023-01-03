0b2b0e491451a37bef2762194a92320ad03e5436
Dennis Ritter <dennis.r.ritter@gmail.com>
2023-01-03 23:18:52 +0100

    add modelnet10 configs

---
+++ scripts/modelnet10_resnet_da.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+DATASET="modelnet10"
+
+PROJECT_NAME="${DATASET}_DA"
+RUN_NAME="resnet_in1k_augmix"
+OUTPUT_DIR="out/synthnet_finetuning/${PROJECT_NAME}"
+
+TRAIN_DS="data/${DATASET}/train"
+# VAL_DS="data/${DATASET}/val"
+TEST_DS="data/${DATASET}/test"
+
+EPOCHS=20
+BATCH_SIZE=16
+LR=3e-5
+WEIGHT_DECAY=0.1
+WARM_UP_RATIO=0.1
+
+python synthnet_vit_finetuning.py \
+--model "microsoft/resnet-50" \
+--project_name $PROJECT_NAME \
+--output_dir $OUTPUT_DIR \
+--train_ds $TRAIN_DS \
+--val_ds $VAL_DS \
+--test_ds $TEST_DS \
+--run_name $RUN_NAME \
+--train_layers "FULL" \
+--seed 42 \
+--batch_size $BATCH_SIZE \
+--num_train_epochs $EPOCHS \
+--learning_rate $LR \
+--weight_decay $WEIGHT_DECAY \
+--warmup_ratio $WARM_UP_RATIO \
+--workers 8 \
+--augmix True
