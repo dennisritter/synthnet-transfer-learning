c5b90613def9f1cfc0208947e7d5a7bba105d548
Dennis Ritter <dennis.ritter@bht-berlin.de>
2023-01-20 12:07:31 +0100

    add setup, reqs, project-root and pre-commit-config to project

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from setuptools import find_packages, setup
+
+setup(
+    name="src",
+    version="0.0.1",
+    description="Transfer learning for the Synthnet project",
+    author="Dennis Ritter",
+    author_email="dennis.ritter@bht-berlin.de",
+    url="https://gitlab.beuth-hochschule.de/iisy/SynthNet/synthnet-transfer-learning",
+    packages=find_packages(),
+)
