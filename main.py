8610155ab2e7e73b7c4bcb840ab15ec7fc0593e8
Dennis Ritter <dennis.ritter@bht-berlin.de>
2023-01-20 12:08:18 +0100

    add src directory with modules; move some files

--- main.py
+++
@@ -1,6 +0,0 @@
-""" The main run script."""
-
-import lightning as pl
-import data_modules
-
-pl.seed_everything(0)
---
+++ src/train.py
@@ -0,0 +1,12 @@
+""" The main run script."""
+
+import lightning as pl
+from datamodules.baseline_finetune_dm import BaselineFinetuneDM
+from datamodules.generic_finetune_dm import GenericFinetuneDM
+
+pl.seed_everything(0)
+dm = GenericFinetuneDM(
+    train_dir="data/visda2017/train",
+    val_dir="data/visda2017/val",
+    test_dir="data/visda2017/test",
+)
