485c59069883192cb2511ba7637a5fa7959d2d3d
Dennis Ritter <dennis.ritter@bht-berlin.de>
2023-01-20 17:11:57 +0100

    start train.py WIP

--- src/train.py
+++ src/train.py
@@ -1,13 +1,41 @@
 """The main run script."""
 
-import lightning as pl
+from typing import List, Optional, Tuple
+
+import hydra
+import pyrootutils
+import pytorch_lightning as pl
+from omegaconf import DictConfig, OmegaConf
+from pytorch_lightning import Callback, LightningDataModule, LightningModule, Trainer
+from rich import print
 
 from data.baseline_finetune_dm import BaselineFinetuneDM
 from data.generic_finetune_dm import GenericFinetuneDM
+from utils import logging_utils
 
-pl.seed_everything(0)
-dm = GenericFinetuneDM(
-    train_dir="data/visda2017/train",
-    val_dir="data/visda2017/val",
-    test_dir="data/visda2017/test",
-)
+# pyrootutils.setup_root(__file__, indicator=".project-root", pythonpath=True)
+
+log = logging_utils.get_logger("rich")
+
+# pl.seed_everything(0)
+# dm = GenericFinetuneDM(
+#     train_dir="data/visda2017/train",
+#     val_dir="data/visda2017/val",
+#     test_dir="data/visda2017/test",
+# )
+
+
+def train(cfg: DictConfig):
+    if cfg.get("seed"):
+        pl.seed_everything(cfg.seed, workers=True)
+    log.info(OmegaConf.to_yaml(cfg), extra={"markup": True})
+
+
+@hydra.main(version_base=None, config_path=None, config_name=None)
+def main(cfg: DictConfig) -> None:
+    # train the model
+    train(cfg)
+
+
+if __name__ == "__main__":
+    main()
